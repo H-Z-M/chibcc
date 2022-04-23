09f90e48345df94c3a2516f24599040775661377
shinya u <shinyahazama7@gmail.com>
2022-04-23 10:28:14 -0400

    Add *, / and ()

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -108,7 +109,7 @@ static Token *tokenize()
 			continue;
 		}
 
-		if (*p == '+' || *p == '-')
+		if (ispunct(*p))
 		{
 			cur = cur->next = new_token(TK_PUNCT, p, p + 1);
 			p++;
@@ -120,6 +121,154 @@ static Token *tokenize()
 	return head.next;
 }
 
+typedef enum
+{
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM,
+}	NodeKind;
+
+typedef struct Node Node;
+struct Node
+{
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
+static Node *new_node(NodeKind kind)
+{
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	return node;
+}
+
+static Node *new_binary(NodeKind kind, Node *lhs, Node *rhs)
+{
+	Node *node = new_node(kind);
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+static Node *new_num(int val)
+{
+	Node *node = new_node(ND_NUM);
+	node->val = val;
+	return node;
+}
+
+static Node *expr(Token **rest, Token *tok);
+static Node *mul(Token **rest, Token *tok);
+static Node *primary(Token **rest, Token *tok);
+
+static Node *expr(Token **rest, Token *tok)
+{
+	Node *node = mul(&tok, tok);
+
+	for (;;)
+	{
+		if (equal(tok, "+"))
+		{
+			node = new_binary(ND_ADD, node, mul(&tok, tok->next));
+			continue;
+		}
+		if (equal(tok, "-"))
+		{
+			node = new_binary(ND_SUB, node, mul(&tok, tok->next));
+			continue;
+		}
+		*rest = tok;
+		return node;
+	}
+}
+
+static Node *mul(Token **rest, Token *tok)
+{
+	Node *node = primary(&tok, tok);
+
+	for (;;)
+	{
+		if (equal(tok, "*"))
+		{
+			node = new_binary(ND_MUL, node, primary(&tok, tok->next));
+			continue;
+		}
+		if (equal(tok, "/"))
+		{
+			node = new_binary(ND_DIV, node, primary(&tok, tok->next));
+			continue;
+		}
+		*rest = tok;
+		return node;
+	}
+}
+
+static Node *primary(Token **rest, Token *tok)
+{
+	if (equal(tok, "("))
+	{
+		Node *node = expr(&tok, tok->next);
+		*rest = skip(tok, ")");
+		return node;
+	}
+	if (tok->kind == TK_NUM)
+	{
+		Node *node = new_num(tok->val);
+		*rest = tok->next;
+		return node;
+	}
+	error_tok(tok, "expected an expression");
+	return NULL;
+}
+
+static int depth;
+
+static void push(void)
+{
+	printf("  push %%rax\n");
+	depth++;
+}
+
+static void pop(char *arg)
+{
+	printf("  pop %s\n", arg);
+	depth--;
+}
+
+static void gen_expr(Node *node)
+{
+	if (node->kind == ND_NUM)
+	{
+		printf("  mov $%d, %%rax\n", node->val);
+		return;
+	}
+	gen_expr(node->rhs);
+	push();
+	gen_expr(node->lhs);
+	pop("%rdi");
+	switch ((int)node->kind)
+	{
+		case ND_ADD:
+			printf("  add %%rdi, %%rax\n");
+			return ;
+		case ND_SUB:
+			printf("  sub %%rdi, %%rax\n");
+			return ;
+		case ND_MUL:
+			printf("  imul %%rdi, %%rax\n");
+			return ;
+		case ND_DIV:
+			printf("  cqo\n");
+			printf("  idiv %%rdi\n");
+			return ;
+	}
+	error("invalid expression");
+}
+
 int main(int argc, char **argv)
 {
 	if (argc != 2)
@@ -127,25 +276,16 @@ int main(int argc, char **argv)
 
 	current_input = argv[1];
 	Token *tok = tokenize();
+	Node *node = expr(&tok, tok);
+
+	if (tok->kind != TK_EOF)
+		error_tok(tok, "exptra token");
 
 	printf("  .global main\n");
 	printf("main:\n");
 
-	printf("  mov $%d, %%rax\n", get_number(tok));
-	tok = tok->next;
-
-	while (tok->kind != TK_EOF)
-	{
-		if (equal(tok, "+"))
-		{
-			printf("  add $%d, %%rax\n", get_number(tok->next));
-			tok = tok->next->next;
-			continue;
-		}
-		tok = skip(tok, "-");
-		printf("  sub $%d, %%rax\n", get_number(tok));
-		tok = tok->next;
-	}
+	gen_expr(node);
 	printf("  ret\n");
+	assert(depth == 0);
 	return 0;
 }
